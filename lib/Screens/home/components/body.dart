d4fd1ce3001e4c31f03fdac35cc8abd3140b1dff
Võ Tấn Đạt <91039754+VNKaiSer@users.noreply.github.com>
2024-01-07 20:03:33 +0700

    done recommenede

--- lib/Screens/home/components/body.dart
+++ lib/Screens/home/components/body.dart
@@ -1,11 +1,9 @@
-import 'package:aspen_travel_app/Utils/hex_color.dart';
 import 'package:flutter/cupertino.dart';
-
-import 'package:google_fonts/google_fonts.dart';
 
 import 'category.dart';
 import 'header.dart';
 import 'popular.dart';
+import 'recommended.dart';
 import 'search_bar.dart';
 
 class Body extends StatelessWidget {
@@ -38,51 +36,10 @@ class Body extends StatelessWidget {
                   height: size.height * 0.04,
                 ),
                 const Popular(),
-                Container(
-                    child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text("Recommended",
-                        style: GoogleFonts.montserrat(
-                            fontSize: 18, fontWeight: FontWeight.w700)),
-                    Row(
-                      children: <Widget>[
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Container(
-                              width: size.width * 0.5,
-                              height: size.height * 0.2,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  color: HexColor("F4F4F4")),
-                              child: Stack(
-                                children: <Widget>[
-                                  Container(
-                                    alignment: Alignment.topCenter,
-                                    padding: EdgeInsets.all(size.width * 0.02),
-                                    width: size.width * 0.48,
-                                    child: Image(
-                                      image: AssetImage(
-                                          "assets/images/Explore_Aspen.png"),
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                  Positioned(
-                                      bottom: 0,
-                                      child: Text("Explore Aspen",
-                                          style: GoogleFonts.montserrat(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.w700)))
-                                ],
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    )
-                  ],
-                ))
+                SizedBox(
+                  height: size.height * 0.04,
+                ),
+                const Recommended(),
               ],
             )),
       ),
